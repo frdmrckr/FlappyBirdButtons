67d08de6731250243978f91bac1af66c8972f849
frdmrckr <campbell.mike4@gmail.com>
2014-04-04 20:58:14 -0400

    Time to test the buttons

---
+++ buttons.py
@@ -0,0 +1,38 @@
+'''
+Created on Apr 4, 2014
+
+@author: Mike Campbell
+'''
+from Adafruit_CharLCDPlate import Adafruit_CharLCDPlate
+from time import sleep
+
+lcd = Adafruit_CharLCDPlate()
+lcd.begin(16, 2)
+lcd.clear()
+lcd.backlight(lcd.on)
+btn = (lcd.LEFT, lcd.UP, lcd.DOWN, lcd.RIGHT, lcd.SELECT)
+score = 0
+state = "new"
+
+while True:
+    
+    if state == "new":
+        lcd.message("Sparty Bird!")
+        sleep(1)
+    
+    for b in btn:
+        if lcd.buttonPressed(b):
+            if b is btn[4]:
+                score +=1
+                lcd.clear()
+                lcd.message("Score:"+score)
+                state ="playing"
+                sleep(.05)
+            if b is btn[3]:
+                score = 0
+                lcd.clear()
+                lcd.message("Game Reset")
+                state="new"
+                sleep(.2)
+             )
+            
